xiaomi-sdm660-devs/android_device_xiaomi_jasmine_sprout

wayne: Move goodix firmware to /vendor/firmware
Edit libgf_ca.so to look in /vendor/firmware instead of /system/etc/firmware as it only checked in
/system/etc/firmware and /firmware/image

Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>
Signed-off-by: Subhajeet Muhuri <kenny3fcb@gmail.com>

extract-files.sh
@@ -23,3 +23,11 @@ export DEVICE_BRINGUP_YEAR=2019
 export DEVICE_SPECIFIED_COMMON=wayne-common
 
 ./../../$VENDOR/$DEVICE_SPECIFIED_COMMON/extract-files.sh $@
+
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+AOSIP_ROOT="$MY_DIR"/../../..
+DEVICE_BLOB_ROOT="$AOSIP_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
+
+sed -i "s|/system/etc/firmware|/vendor/firmware\x0\x0\x0\x0|g" "$DEVICE_BLOB_ROOT"/vendor/lib64/libgf_ca.so

dp-stuff/device_xiaomi_wayne

Change-Id: Ief1553682f3edac2b77fca5679866a92f8bdedae

extract-files.sh
@@ -23,3 +23,11 @@ export DEVICE_BRINGUP_YEAR=2018
 export DEVICE_SPECIFIED_COMMON=wayne-common
 
 ./../../$VENDOR/$DEVICE_SPECIFIED_COMMON/extract-files.sh $@
+
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+LINEAGE_ROOT="$MY_DIR"/../../..
+DEVICE_BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
+
+sed -i "s|/system/etc/firmware|/vendor/firmware\x0\x0\x0\x0|g" "$DEVICE_BLOB_ROOT"/vendor/lib64/libgf_ca.so